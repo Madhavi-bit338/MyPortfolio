5d22be144ea8e8153dc3c4e07618e9abf0d095dd
Madhavi-bit338 <madhavimuthamula@gmail.com>
2024-06-27 22:59:30 +0530

    Update AboutCard.js

--- src/components/About/AboutCard.js
+++ src/components/About/AboutCard.js
@@ -8,13 +8,13 @@ function AboutCard() {
       <Card.Body>
         <blockquote className="blockquote mb-0">
           <p style={{ textAlign: "justify" }}>
-            Hi Everyone, I am <span className="purple">Soumyajit Behera </span>
-            from <span className="purple"> Bhubaneswar, India.</span>
+            Hi Everyone, I am <span className="purple">Madhavi Muthamula </span>
+            from <span className="purple"> Ongole, India.</span>
             <br />
-            I am currently employed as a software developer at Juspay.
+            I am an aspiring engineering graduate in the stream of cyber security
             <br />
-            I have completed Integrated MSc (IMSc) in Maths and Computing at BIT
-            Mesra.
+            I have completed  BTech (Cyber Security) in Maths and Computing at Pace
+            ITS.
             <br />
             <br />
             Apart from coding, some other activities that I love to do!
@@ -34,7 +34,7 @@ function AboutCard() {
           <p style={{ color: "rgb(155 126 172)" }}>
             "Strive to build things that make a difference!"{" "}
           </p>
-          <footer className="blockquote-footer">Soumyajit</footer>
+          <footer className="blockquote-footer">Madhavi </footer>
         </blockquote>
       </Card.Body>
     </Card>
